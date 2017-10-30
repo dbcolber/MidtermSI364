265387025824980ba9b5652a12ecaef759928932
Danielle Colbert <dbcolber@umich.edu>
2017-10-29 20:34:23 -0400

    adding version 2 of midterm python code

--- midterm.py
+++ midterm.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -18,6 +18,10 @@ class WikiForm(FlaskForm):
 def not_found(e):
 	return render_template('404error.html')
 
+@app.errorhandler(505)
+def server_error(e):
+	return render_template('505error.html')
+
 @app.route('/form')
 def wiki_form():
 	myform = WikiForm()
@@ -28,17 +32,30 @@ def result():
 	form = WikiForm(request.form)
 	if request.method == 'POST' and form.validate_on_submit():
 		searchword = form.searchword.data
-
 		result = request.args
 		president = searchword
 		president += ' president'
 		pres_data = wikipedia.search(president)
 		wiki_page = wikipedia.WikipediaPage(pres_data[0])
 		pres_summary = wiki_page.summary
-		return render_template('wikisummary.html', result=pres_summary)
-
+		resp = make_response(render_template('wikisummary.html', result=pres_summary))
+		resp.set_cookie('result', 'pres_summary')
+		return resp
 	flash('Please try again!')
 	return redirect(url_for('wiki_form'))
+
+@app.route('/wikitext/<president>', methods=['GET', 'POST'])
+def wiki_text(president):
+	try:
+		president += ' president'
+		pres_data = wikipedia.search(president)
+		wiki_page = wikipedia.WikipediaPage(pres_data[0])
+		pres_links = wiki_page.links
+		pres_content = wiki_page.content
+		return render_template('wikilinks.html', links=pres_links, content=pres_content)
+	except:
+		flash('Please try again!')
+		return redirect(url_for('wiki_form'))
 
 if __name__=='__main__':
     app.run(debug=True)
