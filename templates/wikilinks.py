78cbf36e6abdc667b06a6d25f97b3fa5d4cbbbd0
Danielle Colbert <dbcolber@umich.edu>
2017-10-29 20:34:46 -0400

    adding complete templates

---
+++ templates/wikilinks.py
@@ -0,0 +1,11 @@
+<h2>Wikipedia Links:</h2>
+<ul>
+	{% for link in links %}
+		<li>
+		<h3>{{ link }}</h3></li>
+	{% endfor %}
+</ul>
+
+<a href="http://localhost:5000/form">Back to Home Page</a>
+
+
